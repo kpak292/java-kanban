d88d97994bc637145372041bf7ee3e78247fe39b
kpak292 <konstantinpak89@gmail.com>
2024-09-05 09:34:06 +0500

    Cleared main

--- src/taskmanager/Main.java
+++ src/taskmanager/Main.java
@@ -13,21 +13,7 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        TaskManager manager = Managers.getDefault();
 
-        Task task1 = new Task("Task1", "Description3");
-
-        task1.setStartTime(LocalDateTime.of(2024, 1, 20, 13, 10, 10));
-
-        task1.setDuration(Duration.ofMinutes(100));
-
-        System.out.println(task1.getStartTime());
-        System.out.println(task1.getDuration());
-        System.out.println(task1.getEndTime());
-
-        manager.addTask(task1);
-
-        System.out.println(manager.getAllTasks().getFirst());
 
     }
 }
