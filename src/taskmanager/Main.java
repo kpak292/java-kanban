8d233545be3f4484a66e5da78ecf1d8241b6aebb
kpak292 <konstantinpak89@gmail.com>
2024-09-05 09:40:43 +0500

    Cleared main

--- src/taskmanager/Main.java
+++ src/taskmanager/Main.java
@@ -1,13 +1,6 @@
 package taskmanager;
 
 
-import taskmanager.controller.Managers;
-import taskmanager.model.Task;
-import taskmanager.service.TaskManager;
-
-import java.time.Duration;
-import java.time.LocalDateTime;
-
 public class Main {
 
 
